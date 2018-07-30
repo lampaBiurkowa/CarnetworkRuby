f73734fd1c9a19fd498cb0f98f7af0dfb5ccd92f
Chrzęścibosz <mrocznaklawiatura@gmail.com>
2018-07-30 09:11:59 +0200

    Client added

--- client.rb
+++ client.rb
@@ -1,21 +1,42 @@
 require 'socket'
 
-class Client
+module CarserverRuby
+    class Client
 
-    def initialize
-        @client = TCPSocket.new('', )
+        @messagesToSend = nil
+        @client = nil
+        @connected = nil
+        @sendingThread = nil
+
+        def initialize
+            @messagesToSend = Array.new
+            @client = TCPSocket.new('', )
+            @connected = true
+            @sendingThread = Thread.new { handleSend } 
+        end
+
+        def receive
+            return @client.gets
+        end
+
+        def send(message)
+            @messagesToSend.push(message)
+        end
+
+        def handleSend(timeout = 1)
+            while @connected
+                if @messagesToSend > 0
+                    send(@messagesToSend[0])
+                    @messagesToSend = @messagesToSend.pop(@messagesToSend.length - 2)
+                    sleep(timeout)
+                end
+            end
+        end
+
+        def disconnect
+            @connected = false
+            @client.close
+        end
+
     end
-
-    def receive
-        return @client.gets
-    end
-
-    def send (message)
-        @client.puts(message)
-    end
-
-    def disconnect
-        @client.close
-    end
-
 end
